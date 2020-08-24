16b8e522f78e1af99ed59d6f606ba63ee865477b
Bo Li <2435638L@student.gla.ac.uk>
2020-08-24 18:44:29 +0100

    update setting.py

--- myhealth_project/myhealth_project/settings.py
+++ myhealth_project/myhealth_project/settings.py
@@ -32,7 +32,10 @@ SECRET_KEY = 'e)qk$clxi8@jrwwtx4hv5+=ye*)bj2u*%3le@i0huyvwy7#)u@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['myhealth.pythonanywhere.com']
+ALLOWED_HOSTS = [
+    "myhealth.pythonanywhere.com",
+    '127.0.0.1',
+]
 
 # # Reset password by sending to terminals
 # # During development only
